7a5fbed85f348a3ca429838aa4a9b48eb1ab3628
poncegui <marta.ponceg@gmail.com>
2021-12-28 16:59:36 +0100

    add Part #2; render title and pics on main.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,16 @@
 
 
 //VARIABLES
-
 const input = document.querySelector(".js_inputUser");
 const btnSearch = document.querySelector(".js_btnSearch");
 const btnReset = document.querySelector(".js_reset");
-
+const templateCard = document.getElementById("template-card").content
+const fragmentList = document.createDocumentFragment()
+const listMovies =document.querySelector(".js_results");
 let tvSerieslist= [];
 
-
+//FETCH
+// PREGUNTAR EN TUTORIA PORQUÉ NO FUNCIONA SIN HTTMP
 function handleSearch(ev){
     ev.preventDefault();
     let inputUser = input.value;
@@ -17,8 +19,9 @@ function handleSearch(ev){
     fetch(`https://api.jikan.moe/v3/search/anime?q=`+ inputUser)
     .then(response => response.json())
     .then((moviesData) => {
-        tvSerieslist = moviesData
+        tvSerieslist = moviesData.results
         console.log(tvSerieslist)
+        renderAnimeTvShows();
          
 
     })
@@ -26,7 +29,22 @@ function handleSearch(ev){
 
 }  
 
+//RENDER 
+// NO OLVIDAR PONER REPLACE EN LAS FOTOS CON https://via.placeholder.com/210x295/ffffff/666666/?text=TV
 
+
+function renderAnimeTvShows(){
+    tvSerieslist.forEach(choice => {
+        console.log(choice);
+        templateCard.querySelector("h5").textContent = choice.title
+        templateCard.querySelector("img").setAttribute("src", choice.image_url)
+        const clone = templateCard.cloneNode(true)
+        fragmentList.appendChild(clone)
+    })
+
+    listMovies.appendChild(fragmentList)
+    console.log(fragmentList)
+}
 
 
 //LISTENERS
