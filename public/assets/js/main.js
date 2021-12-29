20fbd3d8aaf9e71f1f574b3b8e1774f4f672f284
poncegui <marta.ponceg@gmail.com>
2021-12-29 17:53:58 +0100

    add preliminary sass & reset function on main js

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,25 +1,26 @@
 'use strict';
 
-// const { series } = require("gulp");
 
 // ****************************************************************************
 // Preguntas SOPORTE;
 // PORQUÉ NO FUNCIONA SIN HTTP
-  // image: newObject.querySelector("img")
-  //Cómo añadir la url de la imagen al objeto creadoo; la coge pero no la mete en scr  :( 
+  //Cómo añadir la url de la imagen al objeto creadoo; la coge pero no la muestra
+  
+  
   // ****************************************************************************
     // Pendientes;
-    // Crear funcion por si no encuentra la series
+    // Crear mensaje por si no encuentra la series
     // Añadir replace a las búsquedas sin imagenes
 
 
-//VARIABLES
+//GLOBAL VARIABLES
 const input = document.querySelector(".js_inputUser");
 const btnSearch = document.querySelector(".js_btnSearch");
-const btnReset = document.querySelector(".js_reset");
+const btnReset = document.querySelector(".js_btnReset");
+const btnRemoveFav = document.querySelectorAll(".js-remove");
 
 
-//VARIABLES TO RENDER DOM 
+//VARIABLES USED TO RENDER DOM WITH FRAGMENT 
 const templateFavorites = document.getElementById("template-favorites").content
 const templateCard = document.getElementById("template-card").content
 const fragmentList = document.createDocumentFragment()
@@ -27,14 +28,15 @@ const listMovies =document.querySelector(".js_results");
 const listFav =document.querySelector(".js_favorites");
 
 
+
 //ARRAYS
-let tvSerieslist= [];
-let fav=[];
+let tvSerieslist = [];
+let fav = [];
 
 
 
-//FETCH
-// PREGUNTAR EN TUTORIA PORQUÉ NO FUNCIONA SIN HTTP
+//FETCH PART #1
+
 function handleSearch(ev){
     ev.preventDefault();
     let inputUser = input.value;
@@ -43,7 +45,6 @@ function handleSearch(ev){
     .then(response => response.json())
     .then((moviesData) => {
         tvSerieslist = moviesData.results
-        // console.log(tvSerieslist)
         renderAnimeTvShows();
          
 
@@ -52,74 +53,84 @@ function handleSearch(ev){
 
 }  
 
-//RENDER 
-// NO OLVIDAR PONER REPLACE EN LAS FOTOS CON https://via.placeholder.com/210x295/ffffff/666666/?text=TV
-
+//RENDER PART #2  *inyect search user results & clear list
 
 function renderAnimeTvShows(){
     tvSerieslist.forEach(choice => {
-        console.log(choice);
         templateCard.querySelector("h5").textContent = choice.title
         templateCard.querySelector("img").setAttribute("src", choice.image_url)
         templateCard.querySelector(".js-favorite").dataset.id = choice.mal_id
+
         const clone = templateCard.cloneNode(true)
         fragmentList.appendChild(clone)
     })
 
     listMovies.appendChild(fragmentList)
-    // console.log(fragmentList)
+    console.log(fragmentList)
+
+
+    //Clear search user
+    btnReset.addEventListener("click", (ev) => {
+        ev.preventDefault
+        listMovies.innerHTML="";
+    
+    })
+  
 }
 
-//ADD TO FAVORITES LIST
+
+//RENDER PART #3   *create a favorite list
+
 // ***Looking for btn selected***
 
-
-
 const favorite = ev => {
-    console.log(ev.target);
-    // console.log(ev.target.classList.contains("js-favorite"))
     if(ev.target.classList.contains("js-favorite")){
-// console.log(ev.target.parentElement);}
     setFavorite(ev.target.parentElement)}
-    ev.stopPropagation();
+    ev.stopPropagation()
 }
 
 
-// ***Need create a new object with id***
-const setFavorite = newObject =>{
-    // console.log(newObject);
+// ***Need create a new object with "id" and add it to favorite list***
+const setFavorite = newObject => {
     const movieFav = {
         id: newObject.querySelector(".js-favorite").dataset.id,
         title: newObject.querySelector("h5").textContent,
-        // image_url: newObject.querySelector("img").setAttribute("src", choice.image_url),
-        // image: newObject.querySelector("img")
-        // PREGUNTAR EN TUTORIA COMO AÑADIR LA IMAGEN 
-    
+        image_url: newObject.querySelector("img").src,
+        
     }
 
-   
-
+// ***New objet is pushed into "fav"
     fav[movieFav] = {...movieFav}
-    console.log(movieFav);
     renderFav()
   
 }
 
 
-//RENDER FAVORITES
+// ***Render new objet into fav list
 
-const renderFav= () =>{
-    console.log(fav);
+const renderFav = () => {
+
+    // console.log(fav);
+    // console.dir(fav);
     Object.values(fav).forEach(movieFav => {
-        // templateFavorites.querySelector("h5").textContent = movieFav.title
         templateFavorites.querySelector("h5").textContent = movieFav.title
-        const clone = templateFavorites.cloneNode(true)
-        fragmentList.appendChild(clone)
+
+        const clone2 = templateFavorites.cloneNode(true)
+        fragmentList.appendChild(clone2)
     })
 
     listFav.appendChild(fragmentList)
+    console.log(listFav)
+   
+    // btnRemoveFav.addEventListener("click", () => {
+    //     listFav.innerHTML="";
+    
+    // })
+
+     
 }
 
+ //Clear fav movie selected
 
 
 //LISTENERS
